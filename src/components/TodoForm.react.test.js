157bfadc99c40f043ad1b55389def3dea068e8b0
Kavin Jey <jeyakavin33@gmail.com>
2021-12-10 12:05:14 -0500

    feat: added testing, going to update readme

---
+++ src/components/TodoForm.react.test.js
@@ -0,0 +1,14 @@
+import renderer from 'react-test-renderer';
+import TodoForm from './TodoForm'
+
+
+test('Check if forms renders correctly', () => {
+      const component = renderer.create(
+          TodoForm
+  );
+    let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+
+})
+
